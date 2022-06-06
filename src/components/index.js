3300f1d0f72004adc11ac3ff2946d2f0a4de5ffc
roman <kysilroman99@gmail.com>
2022-06-06 17:42:23 +0200

    Initial with <TalkView> and <TalkListView> containing

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+/**
+ * Add your components here.
+ * @module components
+ * @example
+ * import Footer from './Footer/Footer';
+ *
+ * export {
+ *   Footer,
+ * };
+ */
+
+ import TalkView from './Views/Talk';
+ import TalkListView from './Views/TalkList';
+ 
+ export { TalkView, TalkListView };
---
+++ src/config.js
@@ -0,0 +1,34 @@
+/**
+ * Add your config changes here.
+ * @module config
+ * @example
+ * export default function applyConfig(config) {
+ *   config.settings = {
+ *     ...config.settings,
+ *     port: 4300,
+ *     listBlockTypes: {
+ *       ...config.settings.listBlockTypes,
+ *       'my-list-item',
+ *    }
+ * }
+ */
+
+ import { TalkListView, TalkView } from './components';
+
+ // All your imports required for the config here BEFORE this line
+ import '@plone/volto/config';
+ 
+ export default function applyConfig(config) {
+   config.views = {
+     ...config.views,
+     contentTypesViews: {
+       ...config.views.contentTypesViews,
+       talk: TalkView,
+     },
+     layoutViews: {
+       ...config.views.layoutViews,
+       talklist_view: TalkListView,
+     }
+   };
+   return config;
+ }
